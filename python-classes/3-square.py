85aba5efb13c2e79381fa525b15199311c1d6577
sananasgrv <sanan.asgarov2023@khazar.org>
2025-12-09 13:41:49 +0400

    commit

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -11,7 +11,7 @@ class Square:
         return self.__size
     @size.setter
     def size(self, value):
-        if isinstance(value, int):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value  < 0 :
             raise ValueError("size must be >= 0")
