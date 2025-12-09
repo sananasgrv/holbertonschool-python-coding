0a2d8f9b6b170f67597ab15982e6565e5e1c763c
sananasgrv <sanan.asgarov2023@khazar.org>
2025-12-09 13:05:28 +0400

    commit

---
+++ python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Commment"""
+
+
+class Square:
+    """Square class"""
+    def __init__(self, size=0):
+        self.__size = size
+        if not type(size) is int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+    def area(self):
+        self.area=self.__size*4
+        return self.area
