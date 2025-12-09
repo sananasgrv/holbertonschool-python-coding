920ba96451d082f18d4ad21cc37df09612a56bed
sananasgrv <sanan.asgarov2023@khazar.org>
2025-12-09 13:00:38 +0400

    commit

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -5,8 +5,8 @@
 class Square:
     """Square class"""
     def __init__(self, size=0):
-            self.__size = size
-            if not type(size) is int:
-                raise TypeError("size must be an integer")
-            elif size < 0:
-                raise ValueError("size must be >= 0")
+        self.__size = size
+        if not type(size) is int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
