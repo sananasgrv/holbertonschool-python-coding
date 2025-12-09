e5f601d87a9c3cfbc9d9d1386e3e6746b527253b
sananasgrv <sanan.asgarov2023@khazar.org>
2025-12-09 13:18:42 +0400

    commit

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -10,10 +10,10 @@ class Square:
     def area(self):
         self.area = self.__size**2
         return self.area
-
     def size(self):
         return self.__size
     def size(self, value):
+        self.__size = value
         if not self.__size is int:
             raise TypeError("size must be an integer")
         elif self.__size < 0 :
