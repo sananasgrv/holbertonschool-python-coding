e76933121ae2eefc43840af5c4c9360726d6ba3c
sananasgrv <sanan.asgarov2023@khazar.org>
2025-12-09 13:29:00 +0400

    commit

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -5,7 +5,7 @@
 class Square:
     """Square class"""
     def __init__(self, size=0):
-        self.__size = size
+        self.size = size
     def size(self):
         return self.__size
     def size(self, value):
